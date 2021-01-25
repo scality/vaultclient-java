d143a4727fc55fb5733145fba5c3a7aa4ed4e7fd
saitharunthodupunuru <saitharun.thodupunuru@scality.com>
2021-01-24 23:49:12 -0800

    S3C-3567 AccountServicesClient extending AmazonWebServiceClient

--- vaultclient/src/main/java/com/scality/vaultclient/services/AccountServicesClient.java
+++ vaultclient/src/main/java/com/scality/vaultclient/services/AccountServicesClient.java
@@ -1,10 +1,43 @@
 package com.scality.vaultclient.services;
 
+import com.amazonaws.AmazonWebServiceClient;
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Response;
+import com.amazonaws.client.AwsSyncClientParams;
+import com.amazonaws.metrics.RequestMetricCollector;
 import com.scality.vaultclient.dto.CreateAccountRequestDTO;
 import com.scality.vaultclient.dto.CreateAccountResponseDTO;
 
-public class AccountServicesClient implements AccountServices {
+public class AccountServicesClient extends AmazonWebServiceClient implements AccountServices {
+
+    /**
+     * Constructs a new AmazonWebServiceClient object using the specified
+     * configuration.
+     *
+     * @param clientConfiguration The client configuration for this client.
+     */
+    public AccountServicesClient(ClientConfiguration clientConfiguration) {
+        super(clientConfiguration);
+    }
+
+    /**
+     * Constructs a new AmazonWebServiceClient object using the specified
+     * configuration and request metric collector.
+     *
+     * @param clientConfiguration    The client configuration for this client.
+     * @param requestMetricCollector optional request metric collector to be used at the http
+     */
+    public AccountServicesClient(ClientConfiguration clientConfiguration, RequestMetricCollector requestMetricCollector) {
+        super(clientConfiguration, requestMetricCollector);
+    }
+
+    protected AccountServicesClient(ClientConfiguration clientConfiguration, RequestMetricCollector requestMetricCollector, boolean disableStrictHostNameVerification) {
+        super(clientConfiguration, requestMetricCollector, disableStrictHostNameVerification);
+    }
+
+    protected AccountServicesClient(AwsSyncClientParams clientParams) {
+        super(clientParams);
+    }
 
     @Override
     public Response<CreateAccountResponseDTO> createAccount(CreateAccountRequestDTO createAccountRequestDTO) {
