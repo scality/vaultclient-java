e7f860975dda12f4b0ae928f91970ca37adb3844
saitharunthodupunuru <saitharun.thodupunuru@scality.com>
2021-01-24 23:39:00 -0800

    S3C-3567 Create Account Request/Response Marshaller in AWS S3 suggested format

---
+++ vaultclient/src/main/java/com/scality/vaultclient/dto/CreateAccountRequestMarshaller.java
@@ -0,0 +1,60 @@
+package com.scality.vaultclient.dto;
+
+import com.amazonaws.protocol.MarshallLocation;
+import com.amazonaws.protocol.MarshallingInfo;
+import com.amazonaws.protocol.MarshallingType;
+import com.amazonaws.protocol.ProtocolMarshaller;
+import com.amazonaws.util.StringUtils;
+import com.scality.vaultclient.services.VaultClientException;
+
+public class CreateAccountRequestMarshaller extends GenericRequestMarshaller{
+
+    private static final MarshallingInfo<String> NAME_BINDING = MarshallingInfo.builder(MarshallingType.STRING).marshallLocation(MarshallLocation.PAYLOAD)
+            .marshallLocationName("name").build();
+    private static final MarshallingInfo<String> EMAILADDRESS_BINDING = MarshallingInfo.builder(MarshallingType.STRING).marshallLocation(MarshallLocation.PAYLOAD)
+            .marshallLocationName("emailAddress").build();
+    private static final MarshallingInfo<Integer> QUOTAMAX_BINDING = MarshallingInfo.builder(MarshallingType.INTEGER).marshallLocation(MarshallLocation.PAYLOAD)
+            .marshallLocationName("quotaMax").build();
+    private static final MarshallingInfo<String> EXTERNALACCOUNTID_BINDING = MarshallingInfo.builder(MarshallingType.STRING).marshallLocation(MarshallLocation.PAYLOAD)
+            .marshallLocationName("externalAccountId").build();
+    private static final String CREATE_ACCOUNT_ACTION = "CreateAccount";
+
+    private static final CreateAccountRequestMarshaller instance = new CreateAccountRequestMarshaller();
+
+    public static CreateAccountRequestMarshaller getInstance() {
+        return instance;
+    }
+
+    public void marshall(CreateAccountRequestDTO createAccountRequestDTO, ProtocolMarshaller protocolMarshaller) {
+
+        super.marshall(createAccountRequestDTO, protocolMarshaller, CREATE_ACCOUNT_ACTION);
+
+        if (StringUtils.isNullOrEmpty(createAccountRequestDTO.getName())) {
+            throw new VaultClientException("accountName is required.");
+        }
+
+        if (StringUtils.isNullOrEmpty(createAccountRequestDTO.getEmailAddress())) {
+            throw new VaultClientException("email is required.");
+        }
+
+        try {
+            protocolMarshaller.marshall(createAccountRequestDTO.getName(), NAME_BINDING);
+            protocolMarshaller.marshall(createAccountRequestDTO.getEmailAddress(), EMAILADDRESS_BINDING);
+
+            if (createAccountRequestDTO.getQuotaMax() < 0) {
+                throw new VaultClientException("Quota must be a non-negative number.");
+            } else if (createAccountRequestDTO.getQuotaMax() > 0) {
+                protocolMarshaller.marshall(createAccountRequestDTO.getQuotaMax(), QUOTAMAX_BINDING);
+            }
+
+            if (createAccountRequestDTO.getExternalAccountId()!=null) {
+                if (StringUtils.isNullOrEmpty(createAccountRequestDTO.getExternalAccountId())) {
+                    throw new VaultClientException("invalid account id supplied.");
+                }
+                protocolMarshaller.marshall(createAccountRequestDTO.getExternalAccountId(), EXTERNALACCOUNTID_BINDING);
+            }
+        } catch (Exception e) {
+            throw new VaultClientException("Unable to marshall request to JSON: " + e.getMessage(), e);
+        }
+    }
+}
