22da985f3c44f70350872278c66da62896fb7b1a
saitharunthodupunuru <saitharun.thodupunuru@scality.com>
2021-03-17 00:24:28 -0700

    feature: S3C-3568 Unit tests for List Accounts

--- vaultclient/src/test/java/com/scality/vaultclient/services/AccountServicesClientTest.java
+++ vaultclient/src/test/java/com/scality/vaultclient/services/AccountServicesClientTest.java
@@ -2,20 +2,26 @@ package com.scality.vaultclient.services;
 
 import com.amazonaws.Request;
 import com.amazonaws.Response;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.http.AmazonHttpClient;
 import com.amazonaws.http.HttpResponse;
 import com.scality.vaultclient.dto.AccountData;
 import com.scality.vaultclient.dto.CreateAccountRequestDTO;
 import com.scality.vaultclient.dto.CreateAccountResponseDTO;
+import com.scality.vaultclient.dto.ListAccountsRequestDTO;
+import com.scality.vaultclient.dto.ListAccountsResponseDTO;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -23,16 +29,18 @@ import static org.mockito.Mockito.*;
 public class AccountServicesClientTest {
 
     // mock vault client
-    protected static AmazonHttpClient amazonHttpClient;
+    protected static AmazonHttpClient createAccountAmazonHttpClient;
+    protected static AmazonHttpClient listAccountAmazonHttpClient;
 
     // dummy Aws credentials
     protected static BasicAWSCredentials basicAWSCredentials;
 
     // Service
-    protected static AccountServicesClient accountServicesClient;
+    protected static AccountServicesClient createAccountMockClient;
+    protected static AccountServicesClient listAccountsMockClient;
 
     // Service with aws creds
-    protected static AccountServicesClient accountServicesClient2;
+    protected static AccountServicesClient accountServicesClient;
 
     private static final String DEFAULT_ACCOUNT_ID = "001583654825";
 
@@ -44,12 +52,20 @@ public class AccountServicesClientTest {
 
     private static final String DEFAULT_EMAIL_ADDR = "xyz@scality.com";
 
+    private static final int DEFAULT_LIST_ACCOUNTS_COUNT = 10;
+    private static final int TEST_LIST_ACCOUNTS_MARKER_VAL = 2;
+    private static final int TEST_LIST_ACCOUNTS_COUNT = 4;
+    private static final int TEST_LIST_ACCOUNTS_COUNT_2 = -1;
+
     private static final String ERR_EMAIL_ADDR_INVALID = "Invalid EmailAddress";
     private static final String ERR_NAME_INVALID = "Invalid Name";
     private static final String ERR_ARN_NULL = "Arn cannot be null";
     private static final String ERR_CREATE_DATE_NULL = "CreateDate cannot be null";
     private static final String ERR_ID_NULL = "Id cannot be null";
+    private static final String ERR_ID_VALUE = "Id not expected value";
     private static final String ERR_CANONICAL_ID_NULL = "CanonicalId cannot be null";
+
+    private static final String ERR_INVALID_ACCOUNT_COUNT = "Invalid Accounts count";
 
 
     private static final CreateAccountRequestDTO createAccountRequestDTO = CreateAccountRequestDTO.builder()
@@ -57,14 +73,26 @@ public class AccountServicesClientTest {
             .name(DEFAULT_ACCOUNT_NAME)
             .build();
 
+    private static final ListAccountsRequestDTO defaultListAccountsRequestDTO = ListAccountsRequestDTO.builder()
+            .build();
+
     @BeforeAll
     public static void init() throws Exception{
 
-        amazonHttpClient = mock(AmazonHttpClient.class);
-        accountServicesClient = new AccountServicesClient(amazonHttpClient);
+        initCreateAccountMocks();
+
+        initListAccountMocks();
+
+        basicAWSCredentials = new BasicAWSCredentials("accesskey", "secretkey");
+
+    }
+
+    private static void initCreateAccountMocks() {
+        createAccountAmazonHttpClient = mock(AmazonHttpClient.class);
+        createAccountMockClient = new AccountServicesClient(createAccountAmazonHttpClient);
 
         //Default Create Account mock response
-        when(amazonHttpClient.execute(any(Request.class), any(), any(), any(),any()))
+        when(createAccountAmazonHttpClient.execute(any(Request.class), any(), any(), any(),any()))
                 .thenAnswer(new Answer<Response>() {
                     @Override
                     public Response<CreateAccountResponseDTO> answer(InvocationOnMock invocation) {
@@ -89,15 +117,58 @@ public class AccountServicesClientTest {
                         return new Response<>(response,null);
                     }
                 });
+    }
 
-        basicAWSCredentials = new BasicAWSCredentials("accesskey", "secretkey");
-        accountServicesClient2 = new AccountServicesClient(amazonHttpClient, basicAWSCredentials);
+    private static void initListAccountMocks() {
+
+        listAccountAmazonHttpClient = mock(AmazonHttpClient.class);
+        listAccountsMockClient = new AccountServicesClient(listAccountAmazonHttpClient);
+
+        //Default Create Account mock response
+        when(listAccountAmazonHttpClient.execute(any(Request.class), any(), any(), any(),any()))
+                .thenAnswer(new Answer<Response>() {
+                    @Override
+                    public Response<ListAccountsResponseDTO> answer(InvocationOnMock invocation) {
+                        Request<ListAccountsRequestDTO> ogReq = invocation.getArgument(0);
+                        ListAccountsRequestDTO request = (ListAccountsRequestDTO) ogReq.getOriginalRequest();
+                        String marker = request.getMarker();
+                        int maxItems = request.getMaxItems();
+                        if(maxItems <= 0){
+                            maxItems = DEFAULT_LIST_ACCOUNTS_COUNT; // Test Accounts count
+                        }
+
+                        int i = 0;
+                        int markerVal = 0;
+                        if(StringUtils.isNotBlank(marker)){
+                            markerVal = Integer.parseInt(marker.substring(marker.length()-1));
+                            // extracting markerVal index at last character
+                        }
+
+                        List<AccountData> accounts = new ArrayList<>();
+                        // Generate Accounts with ids (markerVal + i) to maxItems count
+                        for(; i < maxItems; i++){
+                            AccountData data = new AccountData();
+                            data.setEmailAddress(DEFAULT_EMAIL_ADDR);
+                            data.setName(DEFAULT_ACCOUNT_NAME);
+                            data.setArn(DEFAULT_ARN_STR + DEFAULT_ACCOUNT_NAME +"/\"");
+                            data.setCreateDate(new Date());
+                            data.setId(DEFAULT_ACCOUNT_ID + (i + markerVal)); //setting ID with index
+                            data.setCanonicalId(DEFAULT_CANONICAL_ID);
+
+                            accounts.add(data);
+                        }
+
+                        ListAccountsResponseDTO response = new ListAccountsResponseDTO();
+                        response.setAccounts(accounts);
+                        return new Response<ListAccountsResponseDTO>(response,null);
+                    }
+                });
     }
 
     @Test
     public void testCreateAccount() throws Exception {
 
-        CreateAccountResponseDTO response = accountServicesClient.createAccount(createAccountRequestDTO).getAwsResponse();
+        CreateAccountResponseDTO response = createAccountMockClient.createAccount(createAccountRequestDTO).getAwsResponse();
 
         assertEquals(DEFAULT_EMAIL_ADDR, response.getAccount().getData().getEmailAddress(), ERR_EMAIL_ADDR_INVALID);
         assertEquals(DEFAULT_ACCOUNT_NAME, response.getAccount().getData().getName(), ERR_NAME_INVALID);
@@ -110,7 +181,8 @@ public class AccountServicesClientTest {
     @Test
     public void testCreateAccountWithCredentials() throws Exception {
 
-        CreateAccountResponseDTO response = accountServicesClient2.createAccount(createAccountRequestDTO).getAwsResponse();
+        accountServicesClient = new AccountServicesClient(createAccountAmazonHttpClient, basicAWSCredentials);
+        CreateAccountResponseDTO response = accountServicesClient.createAccount(createAccountRequestDTO).getAwsResponse();
 
         assertEquals(DEFAULT_EMAIL_ADDR, response.getAccount().getData().getEmailAddress(), ERR_EMAIL_ADDR_INVALID);
         assertEquals(DEFAULT_ACCOUNT_NAME, response.getAccount().getData().getName(), ERR_NAME_INVALID);
@@ -129,7 +201,7 @@ public class AccountServicesClientTest {
                 .quotaMax(10)
                 .build();
 
-        CreateAccountResponseDTO response = accountServicesClient.createAccount(createAccountRequestDTO1).getAwsResponse();
+        CreateAccountResponseDTO response = createAccountMockClient.createAccount(createAccountRequestDTO1).getAwsResponse();
 
         assertEquals(DEFAULT_EMAIL_ADDR, response.getAccount().getData().getEmailAddress(), ERR_EMAIL_ADDR_INVALID);
         assertEquals(DEFAULT_ACCOUNT_NAME, response.getAccount().getData().getName(), ERR_NAME_INVALID);
@@ -149,7 +221,7 @@ public class AccountServicesClientTest {
                 .externalAccountId("249349283982")
                 .build();
 
-        CreateAccountResponseDTO response = accountServicesClient.createAccount(createAccountRequestDTO2).getAwsResponse();
+        CreateAccountResponseDTO response = createAccountMockClient.createAccount(createAccountRequestDTO2).getAwsResponse();
         assertEquals(DEFAULT_EMAIL_ADDR, response.getAccount().getData().getEmailAddress(), ERR_EMAIL_ADDR_INVALID);
         assertEquals(DEFAULT_ACCOUNT_NAME, response.getAccount().getData().getName(), ERR_NAME_INVALID);
         assertNotNull(response.getAccount().getData().getArn(), ERR_ARN_NULL);
@@ -186,7 +258,7 @@ public class AccountServicesClientTest {
 
         final String ENTITY_EXISTS_ERR = "The request was rejected because it attempted to create a resource that already exists.";
 
-        when(amazonHttpClient.execute(any(Request.class), any(), any(), any(),any()))
+        when(createAccountAmazonHttpClient.execute(any(Request.class), any(), any(), any(),any()))
                 .thenAnswer(new Answer<Response>() {
                     @Override
                     public Response<CreateAccountResponseDTO> answer(InvocationOnMock invocation) {
@@ -200,7 +272,7 @@ public class AccountServicesClientTest {
                 });
 
         VaultClientException e = assertThrows(VaultClientException.class, () -> {
-            accountServicesClient.createAccount(createAccountRequestDTO);
+            createAccountMockClient.createAccount(createAccountRequestDTO);
         }, "Expected VaultClientException");
         assertEquals(409, e.getStatusCode(), "Expected http status code: 409");
         assertEquals("EntityAlreadyExists", e.getErrorCode(), "Expected error code: EntityAlreadyExists");
@@ -213,7 +285,7 @@ public class AccountServicesClientTest {
     @Test
     public void testCreateAccountError400() throws Exception {
 
-        when(amazonHttpClient.execute(any(Request.class), any(), any(), any(),any()))
+        when(createAccountAmazonHttpClient.execute(any(Request.class), any(), any(), any(),any()))
                 .thenAnswer(new Answer<Response>() {
                     @Override
                     public Response<CreateAccountResponseDTO> answer(InvocationOnMock invocation) {
@@ -231,7 +303,7 @@ public class AccountServicesClientTest {
                     }
                 });
 
-        HttpResponse response = accountServicesClient.createAccount(createAccountRequestDTO).getHttpResponse();
+        HttpResponse response = createAccountMockClient.createAccount(createAccountRequestDTO).getHttpResponse();
 
         assertEquals(400, response.getStatusCode(), "Expected http status code: 409");
 
@@ -239,6 +311,97 @@ public class AccountServicesClientTest {
         init();
     }
 
+    /** List Accounts Test cases **/
+    @Test
+    public void testListAccounts() throws Exception {
+
+        ListAccountsResponseDTO response = listAccountsMockClient.listAccounts(defaultListAccountsRequestDTO).getAwsResponse();
+
+        assertEquals(DEFAULT_LIST_ACCOUNTS_COUNT, response.getAccounts().size(), ERR_INVALID_ACCOUNT_COUNT);
+        assertEquals(DEFAULT_ACCOUNT_NAME, response.getAccounts().get(0).getName(), ERR_NAME_INVALID);
+        assertNotNull(response.getAccounts().get(0).getArn(), ERR_ARN_NULL);
+        assertNotNull(response.getAccounts().get(0).getCreateDate(), ERR_CREATE_DATE_NULL);
+        assertNotNull(response.getAccounts().get(0).getId(), ERR_ID_NULL);
+        assertNotNull(response.getAccounts().get(0).getCanonicalId(), ERR_CANONICAL_ID_NULL);
+    }
+
+    @Test
+    public void testListAccountsWithCredentials() throws Exception {
+
+        accountServicesClient = new AccountServicesClient(listAccountAmazonHttpClient, basicAWSCredentials);
+        ListAccountsResponseDTO response = accountServicesClient.listAccounts(defaultListAccountsRequestDTO).getAwsResponse();
+
+        assertEquals(DEFAULT_LIST_ACCOUNTS_COUNT, response.getAccounts().size(), ERR_INVALID_ACCOUNT_COUNT);
+        assertEquals(DEFAULT_ACCOUNT_NAME, response.getAccounts().get(0).getName(), ERR_NAME_INVALID);
+        assertNotNull(response.getAccounts().get(0).getArn(), ERR_ARN_NULL);
+        assertNotNull(response.getAccounts().get(0).getCreateDate(), ERR_CREATE_DATE_NULL);
+        assertNotNull(response.getAccounts().get(0).getId(), ERR_ID_NULL);
+        assertNotNull(response.getAccounts().get(0).getCanonicalId(), ERR_CANONICAL_ID_NULL);
+        init();
+    }
+
+    @Test
+    public void testListAccountsWithMarker() throws Exception {
+
+        ListAccountsRequestDTO listAccountsRequestDTO = ListAccountsRequestDTO.builder()
+                .marker(DEFAULT_ACCOUNT_ID + TEST_LIST_ACCOUNTS_MARKER_VAL)
+                .build();
+
+        ListAccountsResponseDTO response = listAccountsMockClient.listAccounts(listAccountsRequestDTO).getAwsResponse();
+
+        assertEquals(DEFAULT_LIST_ACCOUNTS_COUNT, response.getAccounts().size(), ERR_INVALID_ACCOUNT_COUNT);
+        assertEquals(DEFAULT_ACCOUNT_ID + TEST_LIST_ACCOUNTS_MARKER_VAL, response.getAccounts().get(0).getId(), ERR_ID_VALUE);
+    }
+
+    @Test
+    public void testListAccountsWithMaxItems() throws Exception {
+
+        ListAccountsRequestDTO listAccountsRequestDTO = ListAccountsRequestDTO.builder()
+                .maxItems(TEST_LIST_ACCOUNTS_COUNT)
+                .build();
+
+        ListAccountsResponseDTO response = listAccountsMockClient.listAccounts(listAccountsRequestDTO).getAwsResponse();
+
+        assertEquals(TEST_LIST_ACCOUNTS_COUNT, response.getAccounts().size(), ERR_INVALID_ACCOUNT_COUNT);
+    }
+
+    @Test
+    public void testListAccountsWithMaxItemsAndMarker() throws Exception {
+
+        ListAccountsRequestDTO listAccountsRequestDTO = ListAccountsRequestDTO.builder()
+                .marker(DEFAULT_ACCOUNT_ID + TEST_LIST_ACCOUNTS_MARKER_VAL)
+                .maxItems(TEST_LIST_ACCOUNTS_COUNT)
+                .build();
+
+        ListAccountsResponseDTO response = listAccountsMockClient.listAccounts(listAccountsRequestDTO).getAwsResponse();
+
+        assertEquals(TEST_LIST_ACCOUNTS_COUNT, response.getAccounts().size(), ERR_INVALID_ACCOUNT_COUNT);
+        assertEquals(DEFAULT_ACCOUNT_ID + TEST_LIST_ACCOUNTS_MARKER_VAL, response.getAccounts().get(0).getId(), ERR_ID_VALUE);
+    }
+
+    @Test
+    public void testListAccountsWithNegativeMaxItems(){
+        ListAccountsRequestDTO listAccountsRequestDTO = ListAccountsRequestDTO.builder()
+                .maxItems(TEST_LIST_ACCOUNTS_COUNT_2)
+                .build();
+
+        assertThrows(SdkClientException.class, () -> {
+            listAccountsMockClient.listAccounts(listAccountsRequestDTO).getAwsResponse();
+        }, "Expected SdkClientException");
+    }
+
+    @Test
+    public void testListAccountsWithNonNullEmptyMarker(){
+        ListAccountsRequestDTO listAccountsRequestDTO = ListAccountsRequestDTO.builder()
+                .marker("")
+                .build();
+
+        assertThrows(SdkClientException.class, () -> {
+            listAccountsMockClient.listAccounts(listAccountsRequestDTO).getAwsResponse();
+        }, "Expected SdkClientException");
+    }
+
+    /** Tests with Actual Vault **/
     @Disabled
     @Test
     @SuppressWarnings( "deprecation" )
@@ -263,6 +426,25 @@ public class AccountServicesClientTest {
         assertNotNull(response.getAccount().getData().getCreateDate(), ERR_CREATE_DATE_NULL);
         assertNotNull(response.getAccount().getData().getId(), ERR_ID_NULL);
         assertNotNull(response.getAccount().getData().getCanonicalId(), ERR_CANONICAL_ID_NULL);
+    }
+
+    @Disabled
+    @Test
+    @SuppressWarnings( "deprecation" )
+    public void testListAccountsWithActualVault() {
+        //"D4IT2AWSB588GO5J9T00": "UEEu8tYlsOGGrgf4DAiSZD6apVNPUWqRiPG0nTB6"
+        AccountServicesClient amazonIdentityManagementClient = new AccountServicesClient(
+                new BasicAWSCredentials("accesskey", "secretkey"));
+
+        amazonIdentityManagementClient.setEndpoint("http://localhost:8600");
+
+        ListAccountsRequestDTO listAccountsRequestDTO = ListAccountsRequestDTO.builder()
+                .maxItems(2)
+                .marker("699335213475")
+                .build();
+        ListAccountsResponseDTO response = amazonIdentityManagementClient.listAccounts(listAccountsRequestDTO).getAwsResponse();
+        assertNotNull(response.getAccounts().size(), "List Accounts empty");
+        assertNotNull(response.getAccounts().get(0).getId(), "List Accounts ID empty");
     }
 
 }
