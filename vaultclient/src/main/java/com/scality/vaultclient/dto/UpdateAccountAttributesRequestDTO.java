66c986686cb0c281cd78c1234b987cebd4d1244a
saitharunthodupunuru <saitharun.thodupunuru@scality.com>
2021-12-06 09:52:06 -0800

    feature: osis-22 Update Account Attributes API and test cases

---
+++ vaultclient/src/main/java/com/scality/vaultclient/dto/UpdateAccountAttributesRequestDTO.java
@@ -0,0 +1,30 @@
+package com.scality.vaultclient.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UpdateAccountAttributesRequestDTO extends com.amazonaws.AmazonWebServiceRequest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @NonNull
+    private String name;
+
+    private Map<String, String> customAttributes;
+
+
+}
---
+++ vaultclient/src/main/java/com/scality/vaultclient/dto/UpdateAccountAttributesRequestMarshaller.java
@@ -0,0 +1,56 @@
+package com.scality.vaultclient.dto;
+
+import com.amazonaws.protocol.MarshallLocation;
+import com.amazonaws.protocol.MarshallingInfo;
+import com.amazonaws.protocol.MarshallingType;
+import com.amazonaws.protocol.ProtocolRequestMarshaller;
+import com.amazonaws.util.StringUtils;
+import com.google.gson.Gson;
+import com.scality.vaultclient.services.VaultClientException;
+
+/**
+ * The type Update account request marshaller extends GenericRequestMarshaller to marshall Update Account Requests.
+ */
+public class UpdateAccountAttributesRequestMarshaller extends GenericRequestMarshaller<UpdateAccountAttributesRequestDTO>{
+
+    private static final MarshallingInfo<String> NAME_BINDING = MarshallingInfo.builder(MarshallingType.STRING).marshallLocation(MarshallLocation.PAYLOAD)
+            .marshallLocationName("name").build();
+    private static final MarshallingInfo<String> CUSTOM_ATTRIBUTES_BINDING = MarshallingInfo.builder(MarshallingType.JSON_VALUE).marshallLocation(MarshallLocation.PAYLOAD)
+            .marshallLocationName("customAttributes").build();
+    private static final String UPDATE_ACCOUNT_ATTRIBUTES_ACTION = "UpdateAccountAttributes";
+
+    private static final UpdateAccountAttributesRequestMarshaller instance = new UpdateAccountAttributesRequestMarshaller();
+
+    public static UpdateAccountAttributesRequestMarshaller getInstance() {
+        return instance;
+    }
+
+    /**
+     * Binds protocolMarshaller with updateAccountAttributesRequestDTO parameters to marshall.
+     *
+     * @param updateAccountAttributesRequestDTO         the request dto of type UpdateAccountAttributesRequestDTO
+     * @param protocolMarshaller the protocol marshaller
+     */
+    @Override
+    public void marshall(UpdateAccountAttributesRequestDTO updateAccountAttributesRequestDTO, ProtocolRequestMarshaller<UpdateAccountAttributesRequestDTO> protocolMarshaller) {
+
+        super.marshall(updateAccountAttributesRequestDTO, protocolMarshaller, UPDATE_ACCOUNT_ATTRIBUTES_ACTION);
+
+        if (StringUtils.isNullOrEmpty(updateAccountAttributesRequestDTO.getName())) {
+            throw new VaultClientException("accountName is required.");
+        }
+
+        try {
+            protocolMarshaller.marshall(updateAccountAttributesRequestDTO.getName(), NAME_BINDING);
+
+            if (updateAccountAttributesRequestDTO.getCustomAttributes()!=null
+                    && !updateAccountAttributesRequestDTO.getCustomAttributes().isEmpty()) {
+                protocolMarshaller.marshall(new Gson().toJson(updateAccountAttributesRequestDTO.getCustomAttributes()), CUSTOM_ATTRIBUTES_BINDING);
+            } else {
+                throw new VaultClientException("CustomAttributes is required in Update Account Attributes.");
+            }
+        } catch (Exception e) {
+            throw new VaultClientException("Unable to marshall request to JSON: " + e.getMessage(), e);
+        }
+    }
+}
