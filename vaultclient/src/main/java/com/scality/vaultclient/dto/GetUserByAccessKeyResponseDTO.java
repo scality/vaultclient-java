84c2d19d74700d3117a4b88c5471caead6760188
Anurag Mittal <anurag.mittal@scality.com>
2023-03-20 14:07:54 +0100

    add javadoc for GetUserByAccessKeyRequest methods
    fixes #VCLTJV-30

--- vaultclient/src/main/java/com/scality/vaultclient/dto/GetUserByAccessKeyResponseDTO.java
+++ vaultclient/src/main/java/com/scality/vaultclient/dto/GetUserByAccessKeyResponseDTO.java
@@ -4,8 +4,16 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * Represents a response to a request to get a user by access key in the Vault system.
+ */
 @Data
 public class GetUserByAccessKeyResponseDTO implements Serializable {
     private static final long serialVersionUID = 620924237899261417L;
+
+    /**
+     * The user.
+     * @see UserData
+     */
     UserData data;
 }
