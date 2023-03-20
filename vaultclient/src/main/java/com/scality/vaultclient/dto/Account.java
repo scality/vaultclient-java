f85094a7c2e57ae9fd0d485115b9349e23411968
Anurag Mittal <anurag.mittal@scality.com>
2023-03-20 12:44:32 +0100

    VCLTJV-30 add javadoc for Account and AccountData

--- vaultclient/src/main/java/com/scality/vaultclient/dto/Account.java
+++ vaultclient/src/main/java/com/scality/vaultclient/dto/Account.java
@@ -4,8 +4,22 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * Represents an account in the Vault system.
+ *
+ * An account is a data transfer object (DTO) that contains information about an account, including
+ * its name, email address, unique identifier, quota, and creation date. The `data` field is an
+ * instance of the `AccountData` class, which contains the account's data in a structured format.
+ *
+ * This class is used primarily for communicating with the Vault API and should not be used directly
+ * by application code.
+ */
 @Data
 public class Account implements Serializable {
     private static final long serialVersionUID = 1L;
+    /**
+     *  The account's data.
+     *  @see AccountData
+     */
     AccountData data;
 }
--- vaultclient/src/main/java/com/scality/vaultclient/dto/AccountData.java
+++ vaultclient/src/main/java/com/scality/vaultclient/dto/AccountData.java
@@ -7,18 +7,57 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Represents the data for an account in the Vault system.
+ *
+ * An `AccountData` object contains the following data for an account:
+ *
+ * - `arn` - The Amazon Resource Name (ARN) for the account.
+ * - `name` - The name of the account.
+ * - `emailAddress` - The email address associated with the account.
+ * - `id` - The unique identifier for the account.
+ * - `quotaMax` - The maximum storage quota for the account in bytes.
+ * - `quota` - The current storage quota usage for the account in bytes.
+ * - `createDate` - The date when the account was created.
+ * - `canonicalId` - The canonical ID for the account.
+ * - `aliasList` - A list of alias names associated with the account.
+ * - `oidcpList` - A list of OpenID Connect Provider (OIDCP) endpoint URLs associated with the account.
+ * - `customAttributes` - A map of custom attributes associated with the account.
+ */
 @Data
 public class AccountData implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    /** The Amazon Resource Name (ARN) for the account. */
     String arn;
+
+    /** The name of the account. */
     String name;
+
+    /** The email address associated with the account. */
     String emailAddress;
+
+    /** The unique identifier for the account. */
     String id;
+
+    /** The maximum storage quota for the account in bytes. */
     int quotaMax;
+
+    /** The current storage quota usage for the account in bytes. */
     int quota; // This will show up only in list accounts response
+
+    /** The date when the account was created. */
     Date createDate;
+
+    /** The canonical ID for the account. */
     String canonicalId;
+
+    /** A list of alias names associated with the account. */
     List<String> aliasList;
+
+    /** A list of OpenID Connect Provider (OIDCP) endpoint URLs associated with the account. */
     List<String> oidcpList;
+
+    /** A map of custom attributes associated with the account. */
     Map<String,String> customAttributes;
 }
